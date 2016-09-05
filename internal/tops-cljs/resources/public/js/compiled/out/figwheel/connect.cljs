81e7b65a3893900d96e61ea70fce27814d31310b
Andrew Sernyak <ndrew.sernyak@gmail.com>
2016-09-05 21:57:37 +0300

    started reorganization of internal tools :evil

--- internal/tops-cljs/resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [tops-cljs.core] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/tops-cljs.core.on-js-reload (apply js/tops-cljs.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'tops-cljs.core/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
